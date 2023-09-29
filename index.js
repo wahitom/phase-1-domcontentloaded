99fd1810d37fa04d48d22681020d3f8ec096fd70
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-30 02:48:57 +0300

    Complete Assignemt

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM has loaded ")
+
+    let p = document.querySelector('#text')
+    p.textContent = "This is really cool!"
+
+    console.log(p)
+})
